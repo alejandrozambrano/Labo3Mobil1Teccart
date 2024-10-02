8f0743c0dff92a3bc37261738ad1e92007b5c238
alejandrozambranolasso <alejandro_zambrano_@hotmail.com>
2024-10-02 19:32:30 -0400

    carga inicial del proyecto

---
+++ app/src/main/java/com/example/labo3teccart/MainActivity2.java
@@ -0,0 +1,30 @@
+package com.example.labo3teccart;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main2);
+
+    }
+
+    public void SaveDinamique2(View view) {
+    }
+
+    public void SaveDinamique2B(View view) {
+    }
+
+    public void saveRapport(View view) {
+    }
+}
