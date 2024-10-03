b7732a85ef0a0ae4fd4593729219b9b58119111b
alejandrozambranolasso <alejandro_zambrano_@hotmail.com>
2024-10-02 21:19:56 -0400

    add of MainActivity.java

--- app/src/main/java/com/example/labo3teccart/MainActivity.java
+++ app/src/main/java/com/example/labo3teccart/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.labo3teccart;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,14 +14,33 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText name;
+    private Button start;
+    private Context ctx;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        this.ctx = this;
+        this.name = (EditText)findViewById(R.id.txtName);
+        this.start = (Button)findViewById(R.id.btnStart);
+
+        this.start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                public void onClick(View view) {
+                    Finance.setNom(name.getText().toString());
+                    Intent i = new Intent((ctx, MainActivity.class);
+                    startActivity(i);
+                }
+            }
+        });
+
     }
 
-    public void SaveDinamique(View view) {
 
-    }
 }
